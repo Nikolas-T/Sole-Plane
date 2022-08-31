47744c1ab3338986c892e6c42e0885cab3a4b781
AjaniBrowne <ajani.browne@yahoo.com>
2022-08-31 13:47:32 -0400

    update
    added in deleted file and exported api options

---
+++ server/models/sneaker.js
@@ -0,0 +1,64 @@
+const { Schema, model } = require('mongoose');
+
+const sneakerSchema = new Schema({
+    shoeName:{
+        type:String,
+        required:true
+    },
+    brand:{
+        type:String,
+        required:true
+    },
+    silhoutte:{
+        type:String,
+        required:true
+    },
+    styleID:{
+        type:String
+    },
+    make:{
+        type:String,
+        required:true
+    },
+    colorway:{
+        type:String,
+        required:true
+    },
+    retailPrice:{
+        type:Number,
+        required:true
+    },
+    thumbnail:{
+        type:String,
+        required:true
+    },
+    releaseDate:{
+        type:String,
+        required:true
+    },
+    description:{
+        type:String
+    },
+    urlKey:{
+        type:String
+    },
+    imageLinks:{
+        type:String
+    },
+    resellLinks:[
+        {
+            type:String,
+            trim:true,
+            required:true
+        }
+    ],
+    lowestResellPrice:{
+        type:Object
+    },
+
+
+});
+
+const Sneakers = model('Sneakers', sneakerSchema);
+
+module.exports = Sneakers;

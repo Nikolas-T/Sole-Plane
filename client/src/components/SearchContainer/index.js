05b85cf8a6492984169fb7804c909b67a4398fff
AjaniBrowne <ajani.browne@yahoo.com>
2022-09-02 12:24:24 -0400

    updates
    fixed api.js to get all products, created search.js to search products

--- client/src/components/SearchContainer/index.js
+++ client/src/components/SearchContainer/index.js
@@ -1,5 +1,7 @@
 import { useState, useEffect } from 'react';
+import SneakerForm from '../SneakerForm';
 import SneakersList from '../SneakersList';
+
 
 // Import our search method
 import searchSneaker from '../utils/API';
@@ -9,21 +11,22 @@ const SearchContainer = () => {
   const [results, setResults] = useState([]);
 
   // Method to get search results and set state
-  const searchKicks = async (query) => {
-    const response = await searchSneaker(query);
+  const searchKicks = async () => {
+    const response = await searchSneaker();
     setResults(response.data.data);
   };
 
   // We want to run this method when the component first loads so that we have images of kittens to display
   // The second argument is the dependency array. This means that this method will only run when the component first loads
   useEffect(() => {
-    searchKicks('');
+    searchKicks();
   }, []);
 
   return (
     <div>
-      {/* Pass our results to the SneakersList component to map over */}
-      <SneakersList results={results} />
+      {/* Pass our results to the SneakerForm component to map over */}
+      <SneakersList sneakers={results} />
+      <SneakerForm /> 
     </div>
   );
 };

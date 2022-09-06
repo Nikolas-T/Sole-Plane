574b3491e68a011990457f736a0f836b5fbb4830
VictorCast01 <victorcastellon01@gmail.com>
2022-09-06 19:20:54 -0400

    containerStrict

--- client/src/components/Header/Navbar/NavTabs.js
+++ client/src/components/Header/Navbar/NavTabs.js
@@ -5,7 +5,7 @@ import Adidas from '../../../images/white-adidas-logo-transparent-11.png'
 import YEEZY from '../../../images/Daco_5179936.png'
 import Nike from '../../../images/white-nike-logo.png'
 import Jordan from '../../../images/michael-jordan-logo-2671.png'
-import SoulPlane from '../../../images/SoulPlaneYTLogo.png';
+import SolePlane from '../../../images/SOLEPLANELOGO.png';
 import '../../../styles/navbar.css'
 
 function NavTabs({ currentPage, handlePageChange }) {
@@ -17,7 +17,7 @@ function NavTabs({ currentPage, handlePageChange }) {
         style={{ textDecoration: 'none' }}
         className={currentPage === 'Adidas' ? 'nav-link active' : 'nav-link'}
       >
-        <img src={Adidas}  alt="Adidas" height="80" width="80" />
+        <img src={Adidas}  alt="Adidas" height="8%" width="5%" />
       </Link>
     </span>
     <span class='btn-hover color-6'>
@@ -26,7 +26,7 @@ function NavTabs({ currentPage, handlePageChange }) {
         style={{ textDecoration: 'none' }}
         className={currentPage === 'Yeezy' ? 'nav-link active' : 'nav-link'}
       >
-        <img src={YEEZY}  alt="Yeezy" height="60" width="150" />
+        <img src={YEEZY}  alt="Yeezy" height="6%" width="11%" />
       </Link>
     </span>
     <span class='btn-hover color-6'>
@@ -35,7 +35,7 @@ function NavTabs({ currentPage, handlePageChange }) {
         style={{ textDecoration: 'none' }}
         className={currentPage === 'Nike' ? 'nav-link active' : 'nav-link'}
       >
-        <img src={Nike}  alt="Nike" height="60" width="120" />
+        <img src={Nike}  alt="Nike" height="6%" width="7%" />
       </Link>
     </span>
     <span class='btn-hover color-6'>
@@ -44,16 +44,16 @@ function NavTabs({ currentPage, handlePageChange }) {
         style={{ textDecoration: 'none' }}
         className={currentPage === 'Jordan' ? 'nav-link active' : 'nav-link'}
       >
-        <img src={Jordan}  alt="Jordan" height="100" width="100" />
+        <img src={Jordan}  alt="Jordan" height="9%" width="5%" />
       </Link>
     </span>
     <span class='btn-hover color-6'>
       <Link 
-        to='/soulplane' 
+        to='/soleplane' 
         style={{ textDecoration: 'none' }}
-        className={currentPage === 'SoulPlane' ? 'nav-link active' : 'nav-link'}
+        className={currentPage === 'SolePlane' ? 'nav-link active' : 'nav-link'}
       >
-        <img src={SoulPlane}  alt="SoulPlane" height="80" width="100" />
+        <img src={SolePlane}  alt="SolePlane" height="7%" width="5%" />
       </Link>
     </span>
     </ul>
